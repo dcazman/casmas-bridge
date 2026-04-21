fef056619959ae73076f0e2aa31970c874acbebb
Dan Casmas <dcasmas@gmail.com>
2026-04-21 18:35:51 -0400

    feat(main): set scroll restoration and initial scroll position for improved navigation

--- anchor3/client/src/main.jsx
+++ anchor3/client/src/main.jsx
@@ -2,4 +2,6 @@ import { render } from 'preact';
 import { App } from './App';
 import './styles.css';
 
+history.scrollRestoration = 'manual';
+window.scrollTo(0, 0);
 render(<App />, document.getElementById('app'));
